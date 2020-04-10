14b0b044ecfbfa176fe0b2448726cd69ea737bd4
Dovuq <61566919+Dovuq@users.noreply.github.com>
2020-04-10 10:22:03 +0800

    Add files via upload

--- ding.cpp
+++ ding.cpp
@@ -1,15 +1,24 @@
 #include <cstdlib>
+#include <cstdio>
+#include <iostream>
 #include <windows.h>
 using namespace std;
-int main() {
-    system("pause");
+int main()
+{
+    int x,y,n;
+    printf ("Input your x-coordinate: ");
+    scanf ("%d",&x);
+    printf ("Input your y-coordinate: ");
+    scanf ("%d",&y);
+    printf ("Set the number of likes (No more than 100000 recommended): ");
+    scanf ("%d",&n);
+    system ("pause");
     Sleep(2000);
-    SetCursorPos(1330, 840);
-    for (int i=0;i<10000;i++)
-    {
+    SetCursorPos (x,y);
+    while (n--)
+	{
         mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
     }
     return 0;
 }
-// Copyright by xrdrsp and Dovuq.
